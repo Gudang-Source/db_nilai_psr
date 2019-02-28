5a4cf323c58f97133fe67f6b30a9d6a44961a404
Purwa Sabrang Ramadhan <26319163+TheSkinnyRat@users.noreply.github.com>
2019-02-28 19:06:29 +0700

    v1.0

---
+++ assets/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  <title>DB NILAI</title>
+</head>
+
+<body bgcolor="green">
+  <center>
+    <h1>DB NILAI</h1></center>
+  <hr>
+
+  <div class="header">
+    <a href="index.php"><button class="btn-menu">HOME</button></a> --
+    <a href="nilai.php"><button class="btn-menu">LIHAT NILAI</button></a>
+  </div>
---
+++ page/admin/assets/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  <title>DB NILAI</title>
+</head>
+
+<body bgcolor="green">
+  <center>
+    <h1>PENILAIAN</h1></center>
+  <hr>
+
+  <div class="header">
+    <a href="index.php"><button class="btn-menu">HOME</button></a> --
+    <a href="admin.php"><button class="btn-menu">DATA ADMIN</button></a>
+    <a href="guru.php"><button class="btn-menu">DATA GURU</button></a>
+    <a href="siswa.php"><button class="btn-menu">DATA SISWA</button></a> --
+
+    <a href="kelas.php"><button class="btn-menu">DATA KELAS</button></a>
+    <a href="jurusan.php"><button class="btn-menu">DATA JURUSAN</button></a> --
+
+    <a href="mapel.php"><button class="btn-menu">DATA MAPEL</button></a>
+    <a href="mengajar.php"><button class="btn-menu">DATA MENGAJAR</button></a> --
+
+    <a href="action/logout.php"><button class="btn-menu">LOGOUT</button></a>
+  </div>
